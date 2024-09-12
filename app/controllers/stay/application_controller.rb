2d49551bad80caaaed21f6916d49150b1600fac3
khushi <khushi.gupta@w3villa.com>
2024-09-12 13:31:15 +0530

    Login, signup and reset password from both api and view

--- app/controllers/stay/application_controller.rb
+++ app/controllers/stay/application_controller.rb
@@ -1,4 +1,13 @@
 module Stay
   class ApplicationController < ActionController::Base
+
+    def after_sign_in_path_for(resource)
+      if resource.has_role?('admin')
+        admin_dashboard_path
+      else
+        root_path
+      end
+    end
+
   end
 end
