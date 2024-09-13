ad5c38843419d9ba2cdf46141195f3144e649dbb
Renu Vishwakarma <renu.vishwakarma@w3villa.com>
2024-09-13 13:26:58 +0530

    Sign in , sign up layout and different layout for admin and normal user

--- app/controllers/stay/application_controller.rb
+++ app/controllers/stay/application_controller.rb
@@ -1,5 +1,6 @@
 module Stay
   class ApplicationController < ActionController::Base
+    layout :set_layout
 
     def after_sign_in_path_for(resource)
       if resource.has_role?('admin')
@@ -9,5 +10,14 @@ module Stay
       end
     end
 
+    private
+     
+    def set_layout
+      if self.class.name.start_with?('Stay::Admin::')
+        "stay/admin"
+      else
+        "stay/application"
+      end
+    end  
   end
 end
