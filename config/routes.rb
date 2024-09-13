5f16a0e7c738e9336b012c4dd6f038f6a299d83e
khushi <khushi.gupta@w3villa.com>
2024-09-13 12:42:19 +0530

    conflicts resolved

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Stay::Engine.routes.draw do
     sessions: 'stay/users/sessions',
     passwords: 'stay/users/passwords'}
 
+  root to: '/'
+
     namespace :admin do
     get '/', to: 'dashboard#index'
     resources :addresses
@@ -18,4 +20,17 @@ Stay::Engine.routes.draw do
     resources :chats
     resources :messages
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :properties, only: [:index, :show] do
+        resources :rooms, only: [:index, :show] do
+          resources :bookings, only: [:create, :show, :update]
+        end
+      end
+      
+      resources :profiles , only: [:show, :update]
+    end
+  end
+
 end
