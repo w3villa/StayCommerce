ae626b7c6ff3c883f0cfd000842dedeb35df304b
w3villa-vikaspal <vikas.pal@w3villa.com>
2024-09-16 14:56:17 +0530

    structure assets and js for admin and enale jquery and flatpicker for time

--- lib/stay/engine.rb
+++ lib/stay/engine.rb
@@ -1,5 +1,9 @@
 module Stay
   class Engine < ::Rails::Engine
     isolate_namespace Stay
+
+    initializer 'stay.assets.precompile' do |app|
+      app.config.assets.precompile += %w( stay/application.js stay/application.css )
+    end
   end
 end
