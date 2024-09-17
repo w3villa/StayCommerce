b14a66f0d30f16ce4bc54cef4b6a88afe230d0bc
w3villa-vikaspal <vikas.pal@w3villa.com>
2024-09-17 10:08:19 +0530

    apply date time on properties page

--- app/assets/javascripts/stay/custom.js
+++ app/assets/javascripts/stay/custom.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-  flatpickr("input[data-behavior='flatpickr']", {
-    enableTime: false, // Only date picker, no time
-    dateFormat: "Y-m-d", // Date format (e.g., 2024-09-16)
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
   // Admin-specific JS here
   console.log('Admin JS loaded');
   // You can initialize Select2 here as well
   $('select').select2();
+
+  flatpickr("input[data-behavior='flatpickr']", {
+    enableTime: false, // Only date picker, no time
+    dateFormat: "Y-m-d", // Date format (e.g., 2024-09-16)
+    monthSelectorType: "dropdown" // Enable month dropdown selector
+  });
 });
 
