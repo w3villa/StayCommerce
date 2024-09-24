00c4924842be1c4c073db67d3add774275001291
w3villa-pawan <pawan.sharma@w3villa.com>
2024-09-24 18:13:54 +0530

    add validation on role name

--- app/models/stay/role.rb
+++ app/models/stay/role.rb
@@ -2,6 +2,7 @@ module Stay
   class Role < ApplicationRecord
     has_many :role_users, class_name: 'Stay::RoleUser', dependent: :destroy
     has_many :users, through: :role_users, class_name: 'Stay::User'
+    validates :name, presence: true
 
     USER = 'user'.freeze
   end
