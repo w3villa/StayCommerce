c0b368b2ad6093a344182b4e9e6c22f2d18b1316
w3villa-shefali <shefali.goyal@w3villa.com>
2024-10-15 17:53:26 +0530

    Add Room Amenities And Features

--- app/models/stay/amenity.rb
+++ app/models/stay/amenity.rb
@@ -3,5 +3,10 @@ module Stay
     belongs_to :amenity_category, class_name: "Stay::AmenityCategory"
     has_many :property_amenities, class_name: "Stay::PropertyAmenity"
     has_many :properties, through: :property_amenities, class_name: "Stay::Property"
+
+    has_many :room_amenities, class_name: "Stay::RoomAmenity"
+    has_many :rooms, through: :room_amenities, class_name: "Stay::Room"
+
+    validates :name, presence: true
   end
 end
--- app/models/stay/room_feature.rb
+++ app/models/stay/room_feature.rb
@@ -1,6 +1,6 @@
 module Stay
   class RoomFeature < ApplicationRecord
-    belongs_to :room, class_name: "Stay:Room"
-    belongs_to :feature, class_name: "Stay:feature"
+    belongs_to :room, class_name: "Stay::Room"
+    belongs_to :feature, class_name: "Stay::Feature"
   end
 end
