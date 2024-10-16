de246f58e2605becc098932c67c6ba38b21d6f65
deepanshu kashyap <deepanshu@deepanshus-MacBook-Pro.local>
2024-10-16 22:57:54 +0530

    Fix The Changes

--- db/migrate/20241016153847_add_state_to_properties.rb
+++ db/migrate/20241016153847_add_state_to_properties.rb
@@ -1,5 +1,7 @@
 class AddStateToProperties < ActiveRecord::Migration[7.2]
   def change
+    remove_column :stay_properties, :state, :string
+    add_column :stay_properties, :state, :string
     add_column :stay_properties, :property_state, :string, default: 'waiting_for_approval'
   end
 end
