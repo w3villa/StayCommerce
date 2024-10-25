d989a293aa193d68675fadf5ec5966e8989e0db4
deepanshu kashyap <deepanshu@deepanshus-MacBook-Pro.local>
2024-10-25 15:33:20 +0530

    fix api's

--- app/controllers/stay/api/v1/features_controller.rb
+++ app/controllers/stay/api/v1/features_controller.rb
@@ -4,7 +4,7 @@ class Stay::Api::V1::FeaturesController < Stay::BaseApiController
   def property
     features = Stay::Feature.property
     if features.any?
-    render json: { message:ActiveModelSerializers::SerializableResource.new(features, each_serializer: PropertyFeatureSerializer) ,success: true }, status: :ok
+    render json: { data:ActiveModelSerializers::SerializableResource.new(features, each_serializer: PropertyFeatureSerializer) ,success: true }, status: :ok
     else
       render json: { error:"no feature found", success: false }, status: :unprocessable_entity
     end
@@ -13,7 +13,7 @@ class Stay::Api::V1::FeaturesController < Stay::BaseApiController
   def room
     features = Stay::Feature.room
     if features.any?
-      render json: { message:ActiveModelSerializers::SerializableResource.new(features, each_serializer: PropertyFeatureSerializer) ,success: true }, status: :ok
+      render json: { data:ActiveModelSerializers::SerializableResource.new(features, each_serializer: PropertyFeatureSerializer) ,success: true }, status: :ok
     else
       render json: { error:"no feature found", success: false }, status: :unprocessable_entity
     end
