75dd09e993d55386befdd939028028a79954397c
deepanshu kashyap <deepanshu@deepanshus-MacBook-Pro.local>
2024-11-05 14:29:25 +0530

    Add Property Address

--- app/serializers/property_listing_serializer.rb
+++ app/serializers/property_listing_serializer.rb
@@ -1,6 +1,6 @@
 class PropertyListingSerializer < ActiveModel::Serializer
-  attributes :id, :title, :price_per_night, :place_images, :price_per_night, :total_rooms,
-              :availability_end, :availability_start, :extra_guest, :latitude, :longitude
+  attributes :id, :title, :price_per_night, :place_images, :total_rooms,
+              :availability_end, :availability_start, :extra_guest, :latitude, :longitude, :address, :city, :state, :country
 
   def place_images
     object.place_images.attached? ? object.place_images_urls : []
