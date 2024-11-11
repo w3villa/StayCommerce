2255150b6eae2a761dca95cd5450b06346d9590d
deepanshu kashyap <deepanshu@deepanshus-MacBook-Pro.local>
2024-11-11 10:38:45 +0530

    Fix Query

--- app/controllers/stay/api/v1/bookings_controller.rb
+++ app/controllers/stay/api/v1/bookings_controller.rb
@@ -115,7 +115,7 @@ class Stay::Api::V1::BookingsController < Stay::BaseApiController
   end
 
   def show
-      render json: { booking: BookingSerializer.new(@booking),BookingQuerySerializer.new(@booking&.booking_query)  booking_query: success: true}, status: :ok
+      render json: { booking: BookingSerializer.new(@booking),booking_query:BookingQuerySerializer.new(@booking&.booking_query),  success: true}, status: :ok
   end
 
   def update
