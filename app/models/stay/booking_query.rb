b0dbefceac5fc0ffc1eb0e8d8d93f3df0943bfc6
deepanshu kashyap <deepanshu@deepanshus-MacBook-Pro.local>
2024-12-02 12:18:13 +0530

    Restrict-Booking-Query

--- app/models/stay/booking_query.rb
+++ app/models/stay/booking_query.rb
@@ -8,6 +8,9 @@ module Stay
     enum state: { send_message: 0, request_change: 1, booking_invitation: 2, accepted: 3, rejected: 4 }
     after_initialize :set_default_state, if: :new_record?
     scope :ongoing, -> { where.not(state: ACCEPTED) }
+    scope :date_range, ->(check_in, check_out) { where(check_in_date: check_in, check_out_date: check_out) }
+    scope :without_booking, -> { where(booking_id: nil) }
+    scope :for_current_user, ->(user) { where(user: user) }
 
     def set_default_state
       self.state ||= :send_message
