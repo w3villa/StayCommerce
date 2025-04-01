7c68939b141f4ba9d3c80bcf57e92a080a0a9a25
deepanshu kashyap <deepanshu@deepanshus-MacBook-Pro.local>
2025-04-01 11:38:13 +0530

    Remove User Validation

--- app/models/stay/user.rb
+++ app/models/stay/user.rb
@@ -5,7 +5,6 @@ module Stay
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable, :api
 
-    validates :first_name, :last_name, presence: true
     # after_create :assign_default_role
     has_many :role_users, class_name: "Stay::RoleUser", dependent: :destroy
     has_many :stay_roles, through: :role_users, class_name: "Stay::Role", source: :role
